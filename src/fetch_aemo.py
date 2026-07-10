e79edd1ac66dd53d1d1252b746f0b753f460c055
TonyPhame <quan.pham@adelaide.sa.edu.au>
2026-07-10 15:53:31 +0930

    Moved all magic number into constant file for better readability and maintenance, successful postgres connection

--- src/fetch_aemo.py
+++ src/fetch_aemo.py
@@ -7,9 +7,10 @@ import numpy as np
 import pandas as pd
 from nemosis import dynamic_data_compiler, static_table
 
-DEFAULT_END = dt.datetime(2016, 4, 20, 0, 0, 0)
-DEFAULT_START = DEFAULT_END - dt.timedelta(days=10)
-DEFAULT_TABLE = "DISPATCHPRICE"
+from constants import DEFAULT_LOOKBACK_DAYS, DEFAULT_REGION, DEFAULT_TABLE, DROPPED_FCAS_COLUMNS
+
+DEFAULT_END = dt.datetime.now().replace(microsecond=0)
+DEFAULT_START = DEFAULT_END - dt.timedelta(days=DEFAULT_LOOKBACK_DAYS)
 
 
 class NEMDataFetcher:
@@ -100,7 +101,7 @@ class NEMDataFetcher:
         format='%Y/%m/%d %H:%M:%S'
         start=start_time.strftime(format)
         end=end_time.strftime(format)
-        csv=dynamic_data_compiler(start_time=start,end_time=end, table_name=table, raw_data_location=self.__datapath, fformat='csv', filter_cols=['REGIONID'], filter_values=(['SA1'],))
+        csv=dynamic_data_compiler(start_time=start,end_time=end, table_name=table, raw_data_location=self.__datapath, fformat='csv', filter_cols=['REGIONID'], filter_values=([DEFAULT_REGION],))
 
         if csv is None or csv.empty:
             print('Empty data fetched.')
@@ -130,7 +131,7 @@ class NEMDataFetcher:
         # Using pandas to drop the unnecessary columns for now. Only focus on RRP, SETTLEMENTDATE. The REGIONID is SA1 by default.
         df=pd.read_csv(self.__full_csv_filepath,sep=',')
 
-        dropping_cols=['INTERVENTION','RAISE6SECRRP','RAISE60SECRRP','RAISE5MINRRP','RAISEREGRRP','LOWER6SECRRP','LOWER60SECRRP','LOWER5MINRRP','LOWERREGRRP','PRICE_STATUS','REGIONID']
+        dropping_cols=DROPPED_FCAS_COLUMNS
         df.drop(columns=dropping_cols,inplace=True)
 
         filename=os.path.join(self.__datapath, filename)
